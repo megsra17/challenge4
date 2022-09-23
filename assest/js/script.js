0a52982295bfcd74cb73292cc8724ee3c034d815
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-23 18:49:39 -0500

    edits to code

--- assest/js/script.js
+++ assest/js/script.js
@@ -2,9 +2,9 @@ var nextBtn = document.querySelector('button');
 var h3El = document.querySelector('h3');
 var pEl = document.querySelector('.body');
 var section = document.getElementById('section');
+var header = document.getElementById('header');
 var time = document.querySelector('.time');
 var ulEL = document.querySelector('ul')
-
 
 var score = 0
 //Count Down Variables
@@ -35,33 +35,76 @@ var questionList = [
     }
 ]
 
+//renders the new questions
 function renderQuestion(){
-    h3El.innerText = questionList[index].question;
-    questionList[index].choices.innerHTML = " ";
+    var currentQuestion = questionList[index].question;
+    h3El.innerText = currentQuestion
+    ulEL.innerHTML = " ";
     for (var i = 0; i < questionList[index].choices.length; i++){ 
         var li = document.createElement('button');
+        li.addEventListener('click', guessedAnswer)
         li.innerText = questionList[index].choices[i];
         console.log(li.innerText = questionList[index].choices[i])
         ulEL.appendChild(li);
     }
 }
 
-function guessedAnswer(){
-    if ( === questionList[index].correct){
+//answers
+function guessedAnswer(event){   
+    //if no questions left
+     if(!questionList[index]){
+        score = timeLeft;
+        GameOver()
+    }else{
+        
+    }
+    //checks anwser
+     if ( event.target.innerText === questionList[index].correct){
         score++
     } else{
         timeLeft -= 10;
         time.textContent = timeLeft;
     }
-    renderQuestion()++;
+    index++
+   renderQuestion()
 }
 
+//game over screen
 function GameOver(){
     clearInterval(countDown);
+    section.removeChild(ulEL);
         h3El.innerText = ("Game Over");
+        section.appendChild(pEl)
         pEl.innerText = ('Your Score was: ' + score);
+        var form = document.createElement('form');
+        form.setAttribute('method',"post");
+        var input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('id', 'initials')
+        input.setAttribute('placeholder', 'Enter Your Intials');
+        var submit = document.createElement('input');
+        submit.setAttribute('type', 'submit');
+        submit.setAttribute('value', 'Submit');
+        form.appendChild(input);
+        form.appendChild(submit);
+        section.appendChild(form);
+        var inputInitials = document.getElementById('initials')
+        localStorage.setItem("initials", inputInitials.value);
+        console.log(localStorage.setItem("iniitals", inputInitials.value))
+        console.log(localStorage.getItem("initials"))
+        
+        submit.addEventListener('click', function(event){
+            event.preventDefault();
+            h3El.innerText = ("High Scores");
+            section.removeChild(form)
+            console.log(localStorage.getItem("initials"))
+            var highScore = localStorage.getItem("initials")
+            pEl.innertext = ("High Score" + highScore + score);
+
+        })
 }
 
+//starts timer
 function startCountDown(){
     clearInterval(countDown)
     countDown =  setInterval(function() {
@@ -77,24 +120,13 @@ function startCountDown(){
 
 
 nextBtn.addEventListener('click', function(){
-    //section.removeChild(pEl)
-   //section.removeChild(nextBtn)
+    section.removeChild(pEl)
+   section.removeChild(nextBtn)
     index ++
-    startCountDown()
     renderQuestion()
+    startCountDown()
     
-    
-
 })
 
-
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
